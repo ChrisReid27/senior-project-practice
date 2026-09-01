07ed846e2450f88fc96d6b5b43a6867db1e5411f
Christopher Reid <168359463+ChrisReid27@users.noreply.github.com>
2026-09-01 13:32:31 -0400

    Initial developer profile created
    My initial developer profile was created showing my name, major, tech interest, and skill goal for senior project 1.

---
+++ app.py
@@ -0,0 +1,12 @@
+# Senior project practice app
+title = "Senior Project Developer Profile"
+name = "Christopher Reid"
+major = "Computer Science"
+tech_interest = "Software Development"
+skill_goal = "I want to improve my coding skills, " \
+"especially in non-python language if I get the chance."
+print(title)
+print(f"Name: {name}")
+print(f"Major: {major}")
+print(f"Tech Interest: {tech_interest}")
+print(f"Skill Goal: {skill_goal}")
